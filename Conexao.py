d3bb60c73628bfa66a925bfd0808cbf953b9adb0
Carlos Achucarro <cachucarro6@gmail.com>
2026-04-14 20:51:15 -0300

    Fix formatting issues in Conexao.py

--- Conexao.py
+++ Conexao.py
@@ -2,7 +2,7 @@ import os
 import sqlalchemy
 import sqlalchemy.orm
 
-DATABASE_URL = os.getenv("mysql=pymysql://root:llVIhzbGjWIcFDhmRLtRXiACNztEJbDF@centerbeam.proxy.rlwy.net:44206/railway")
+DATABASE_URL = os.getenv("mysql+pymysql://root:llVIhzbGjWIcFDhmRLtRXiACNztEJbDF@centerbeam.proxy.rlwy.net:44206/railway")
 
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL environment variable is not set.")
