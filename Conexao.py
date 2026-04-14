2ba3d55ae8f606e343c99e83b3a75ab3af3105cc
Carlos Achucarro <cachucarro6@gmail.com>
2026-04-14 19:49:09 -0300

    Refactor database connection to use environment variable
    Updated DATABASE_URL to use environment variable and added error handling.

--- Conexao.py
+++ Conexao.py
@@ -1,7 +1,11 @@
+import os
 import sqlalchemy
 import sqlalchemy.orm
 
-DATABASE_URL = "mysql+pymysql://root:admin@localhost/cashback"
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable is not set.")
 
 engine = sqlalchemy.create_engine(DATABASE_URL)
 SessionLocal = sqlalchemy.orm.sessionmaker(bind=engine)
